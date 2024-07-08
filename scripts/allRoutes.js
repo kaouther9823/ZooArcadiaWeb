d30c27910172b5ee149ab8f93622eb42609e40e5
kaouther9823 <159493657+kaouther9823@users.noreply.github.com>
2024-07-08 11:57:27 +0200

    Feature/us2 (#4)
    * us2: menu de l'application
    
    * ajout page contact

--- scripts/allRoutes.js
+++ scripts/allRoutes.js
@@ -2,7 +2,10 @@ import Route from "./Route.js"
 
 export const  allRoutes= [
     new Route("/", "Accueil", "pages/home.html"),
-    new Route("/animals", "Animaux", "pages/animals.html"),
+    new Route("/habitats", "Habitats", "pages/habitats.html", "scripts/entities/habitat.js", "/api/habitats", true),
+    new Route("/services", "Services", "pages/services.html", "scripts/entities/service.js", "/api/services", true),
+    new Route("/contact", "Contact", "pages/contact.html"),
+    new Route("/connexion", "Connexion", "pages/connexion.html"),
 ]
 
 export const websiteName = "Zoo Arcadia";
