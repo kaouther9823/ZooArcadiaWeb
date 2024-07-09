b086c720d941e99fe54626c82c327b3f1c10043a
kaouther9823 <159493657+kaouther9823@users.noreply.github.com>
2024-07-09 02:58:09 +0200

    us3: crud service (#5)

--- scripts/allRoutes.js
+++ scripts/allRoutes.js
@@ -2,10 +2,12 @@ import Route from "./Route.js"
 
 export const  allRoutes= [
     new Route("/", "Accueil", "pages/home.html"),
-    new Route("/habitats", "Habitats", "pages/habitats.html", "scripts/entities/habitat.js", "/api/habitats", true),
-    new Route("/services", "Services", "pages/services.html", "scripts/entities/service.js", "/api/services", true),
+    new Route("/habitats", "Habitats", "pages/habitats.html", "scripts/visiteur/habitat.js", "/api/habitats", true),
+    new Route("/services", "Services", "pages/services.html", "scripts/visiteur/service.js", "/api/services", true),
     new Route("/contact", "Contact", "pages/contact.html"),
     new Route("/connexion", "Connexion", "pages/connexion.html"),
+    new Route("/admin/services", "Services", "/pages/admin/services.html", "/scripts/entities/service.js", "/api/services", true),
+    new Route("/admin/habitats", "Habitats", "/pages/admin/habitats.html", "/scripts/entities/habitat.js", "/api/habitats", true),
 ]
 
 export const websiteName = "Zoo Arcadia";
